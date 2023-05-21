015f472827db8e5d92bf872a9985a47b6a546e48
Oleg Plugaru <oleg.plugaru.md@gmail.com>
2023-05-21 16:02:33 +0300

    Initial Commit

---
+++ Date-Picker/Date_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Date_PickerApp.swift
+//  Date-Picker
+//
+//  Created by Whoami on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Date_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
