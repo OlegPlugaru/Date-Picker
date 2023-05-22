033bd87cb5ddf9cf99761f908a7780c3000ab5bf
Oleg Plugaru <oleg.plugaru.md@gmail.com>
2023-05-22 20:44:48 +0300

    finished

--- Date-Picker/ContentView.swift
+++ Date-Picker/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var birthdate = Date()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            DatePicker("Birthdate", selection: $birthdate)
+                .datePickerStyle(GraphicalDatePickerStyle())
         }
-        .padding()
     }
 }
 
